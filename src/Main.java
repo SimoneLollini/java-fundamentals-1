d2a7b7ef06dabf03a26e9f864f75a34a80e014f6
SimoneLollini <lollinisimone@gmail.com>
2023-03-08 14:12:21 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+//        aggiungete il package org.lessons.java. Nel package create due classi distinte, una per ogni esercizio. Ciascuna classe ha il suo metodo main per poterla eseguire.
+
+//        Nome repo: java-fundamentals-1
+//        Esercizio 1: Calcolo biglietto del treno
+//        Consegna: Il programma dovrà chiedere all’utente il numero (intero) di chilometri che vuole percorrere e l’età del passeggero (numero intero). Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//        il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//        va applicato uno sconto del 20% per i minorenni
+//        va applicato uno sconto del 40% per gli over 65
+//        Per chiedere informazioni all’utente sarà necessario usare la classe java.util.Scanner, come visto in classe stamattina.
+//        Bonus: formattare il risultato con un numero limitato di decimali utilizzando la classe DecimalFormat
+
+
+//        Esercizio 2: FizzBuzz
+//        Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+//        Esempio: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz……
+//        Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)
+    }
+}
